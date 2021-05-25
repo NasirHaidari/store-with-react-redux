04f87fdfbcdec27e95f5598065cb43a6fe19e0fe
NasirHaidari <nasir.haidri@htomail.com>
2021-05-26 01:21:48 +0200

    implementet redux in test propose

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './containers/Headers'
+import ProductListing from './containers/ProductListing'
+import ProductDetail from './containers/ProductDetail'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        {' '}
+        <Header />
+        <Switch>
+          <Route exact path='/' component={ProductListing} />
+          <Route path='/product/:productId' component={ProductDetail} />
+          <Route>404 Error</Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
